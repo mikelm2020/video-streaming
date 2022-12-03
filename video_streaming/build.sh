6261ba7a360efae31c4372dd018879e2a2fe5f2b
Miguel Angel López <admon@tecnott.com.mx>
2022-12-03 08:24:23 -0600

    Changes in configuration for production

--- video_streaming/build.sh
+++ video_streaming/build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 python -m pip install --upgrade pip
-pip install -r requirements/prod.txt
+pip install -r ..requirements/prod.txt
 
-cd video_streaming
 python manage.py collectstatic --no-input
 python manage.py migrate
