42f2f33f1be9a06fd8eff162fa0bec0b4a8e80ae
Miguel Angel López <admon@tecnott.com.mx>
2022-12-04 13:23:03 -0600

    test

--- video_streaming/manage.py
+++ video_streaming/manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print("DJANGO_SETTINGS_MODULE: " + os.environ["DJANGO_SETTINGS_MODULE"])
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'video_streaming.settings.local')
     try:
         from django.core.management import execute_from_command_line
