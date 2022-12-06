c0ad0223155070793e1e0bb9bf8c347e00e5ad31
Miguel Angel López <admon@tecnott.com.mx>
2022-12-06 00:20:03 -0600

    Added fixture in build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ pip install -r requirements/requirements.txt
 cd video_streaming
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py flush --noinput
+python manage.py loaddata mydata.json
