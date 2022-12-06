f9f361c1d26cf1b40f81e6bfe8e8b53e87fc97bd
Miguel Angel López <admon@tecnott.com.mx>
2022-12-06 00:24:02 -0600

    Corrected script build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ pip install -r requirements/requirements.txt
 cd video_streaming
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py flush --noinput
 python manage.py loaddata mydata.json
